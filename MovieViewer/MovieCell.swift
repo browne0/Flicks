ead5e8d7b26986dd9e48fa4bade2a07825d78db8
Malik <browne0@purdue.edu>
2016-01-10 22:39:50 -0500

    First commit with files from Assignment #1

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Malik Browne on 1/10/16.
+//  Copyright © 2016 Malik Browne. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
