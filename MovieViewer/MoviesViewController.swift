6241a31d94a0978f4c4ae5d2fd277b025fcb3b0d
Malik <browne0@purdue.edu>
2016-01-25 23:07:47 -0500

    Project 2 commit

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var networkErrorView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var endpoint: String!
     
     var movies: [NSDictionary]?
+    
+    var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.titleTextAttributes = [
+                NSFontAttributeName: UIFont.boldSystemFontOfSize(22),
+                NSForegroundColorAttributeName: UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+            ]
+            navigationBar.tintColor = UIColor(red: 204.0/255.0, green: 225.0/255.0, blue: 232.0/255.0, alpha: 0.8)
+        }
+        
+        self.tableView.contentInset = UIEdgeInsetsMake(-34,0,0,0);
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.frame = view.frame
-        view.addSubview(tableView)
+        searchBar.delegate = self
         
         let topBar = UIView(frame: UIApplication.sharedApplication().statusBarFrame)
         topBar.backgroundColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
@@ -34,27 +47,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
         
+        
                 if Reachability.isConnectedToNetwork() == true
                 {
-                    self.tableView.transform = CGAffineTransformMakeTranslation(0.0, -44.0)
-                    UIView.animateWithDuration(0.2, animations: {
+                    
+                    UIView.animateWithDuration(0.15, animations: {
                         self.networkErrorView.alpha = 0
                         
                         let tableViewWidth = self.tableView.frame.width
                         self.tableView.frame = CGRectMake(0, 0, tableViewWidth, 582)
-                        self.networkErrorView.transform = CGAffineTransformMakeTranslation(0.0, -34.0)
                     })
+                    fetchStories()
                 }
         
                 else if Reachability.isConnectedToNetwork() == false
                 {
                     self.networkErrorView.transform = CGAffineTransformMakeTranslation(0.0, 34.0)
-                    UIView.animateWithDuration(0.4, animations: {
+                    self.tableView.contentInset = UIEdgeInsetsMake(0,0,0,0);
+                    UIView.animateWithDuration(0.15, animations: {
                         self.networkErrorView.alpha = 1
                     })
                 }
-        fetchStories()
-        
         
         // Do any additional setup after loading the view.
     }
@@ -67,7 +80,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func fetchStories()
     {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
@@ -94,9 +107,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func refreshControlAction(refreshControl: UIRefreshControl)
     {
+        if Reachability.isConnectedToNetwork() == true {
+            UIView.animateWithDuration(0.15, animations: {
+                self.networkErrorView.alpha = 0
+                self.tableView.contentInset = UIEdgeInsetsMake(-34,0,0,0);
+                let tableViewWidth = self.tableView.frame.width
+                self.tableView.frame = CGRectMake(0, 0, tableViewWidth, 582)
+                self.networkErrorView.transform = CGAffineTransformMakeTranslation(0.0, -34.0)
+            })
+            
         fetchStories()
         self.tableView.reloadData()
         refreshControl.endRefreshing()
+            
+        }
+        
+        else if Reachability.isConnectedToNetwork() == false {
+            self.tableView.reloadData()
+            refreshControl.endRefreshing()
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -109,13 +138,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
-    func delay(delay:Double, closure:()->()) {
-        dispatch_after(
-            dispatch_time(
-                DISPATCH_TIME_NOW,
-                Int64(delay * Double(NSEC_PER_SEC))
-            ),
-            dispatch_get_main_queue(), closure)
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        movies = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+            return dataString.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -124,27 +151,37 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
+        if let posterPath = movie["poster_path"] as? String {
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            cell.posterView.setImageWithURL(imageUrl!)
+        }
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
-        
-        print("row \(indexPath.row)")
+        cell.selectionStyle = .Blue
+            
         return cell
     }
     
-    /*
+    
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = movies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
+        
+        
     // Get the new view controller using segue.destinationViewController.
     // Pass the selected object to the new view controller.
     }
-    */
+
     
 }
